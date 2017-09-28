668b3c25968ed16de9e46102df97fadfb51abab5
SaraJesperson <sara.jesperson@hotmail.com>
2017-09-28 10:03:50 +0200

    describe package

---
+++ lab5sarasara/R/lab5sarasara.R
@@ -0,0 +1,7 @@
+#' Election results of the Swedish election 2014
+#' 
+#' @description A package containing functions to investigate the election results of the Swedish election 2014
+#' @docType package
+#' @name lab5sarasara
+#' @author Sara Jesperson and Sara Johansson
+NULL
