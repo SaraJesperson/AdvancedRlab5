6507463946f1f0421d285e7242029172601817b3
sarajohansson91 <shjhnssn@gmail.com>
2017-10-02 18:02:51 +0200

    test

--- lab5sarasara/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(lab5sarasara)
-
-test_check("lab5sarasara")
--- lab5sarasara/tests/testthat/test_graph_election.R
+++
@@ -1,9 +0,0 @@
-context("graph_election")
-
-
-
-test_that("grap_election stops with error", {
-  expect_error(graph_election("Sara","Riksdagsval"))
-  expect_error(graph_election("Mora","Landstingvsal"))
-  expect_error(graph_elction(c("Mora","Orsa"),"Kommunval"))
-})
