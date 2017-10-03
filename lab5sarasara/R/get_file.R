1e44225cc9a4dd66ddd2b23ad22c028c4030909e
SaraJesperson <sara.jesperson@hotmail.com>
2017-10-03 15:12:05 +0200

    s

--- lab5sarasara/R/get_file.R
+++ lab5sarasara/R/get_file.R
@@ -19,7 +19,7 @@ get_file<- function(x){
   path<-files[x]
   file<-paste(base, path, sep="")
   
-  result<-list(file=file, table=utils::read.csv2(file, stringsAsFactors=FALSE, encoding="UTF-8", fileEncoding = "latin1"))
+  result<-list(file=file, table=readr::read_csv2(file, locale=readr::locale("se")))
   
   #colnames(result[[2]])<-stringr::str_replace_all(colnames(result[[2]]), pattern=".f6.", replacement="o")
   #colnames(result[[2]])<-stringr::str_replace_all(colnames(result[[2]]), pattern=".e5.", replacement="a")
@@ -28,3 +28,4 @@ get_file<- function(x){
   return(result)
 }
 
+a<-get_file(1)[[2]]
