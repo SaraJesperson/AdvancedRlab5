e63e34f8e87f6402505d82a1ba01f74193a659a0
sarajohansson91 <shjhnssn@gmail.com>
2017-10-06 10:28:07 +0200

    Test

--- lab5sarasara/tests/testthat/test_get_file.R
+++ lab5sarasara/tests/testthat/test_get_file.R
@@ -1,19 +1,21 @@
 context("get_file")
 
 
-test_that("Collects the correct file", {
- expect_output(str(get_file()[2,4]), "Karlskrona")
-#  expect_output(str(get_file(12)), "http://www.val.se/val/val2014/alkon/alkandur_L.skv")
-#  expect_output(str(get_file(17)), "http://www.val.se/val/val2014/alkon/L/rostberattigade.skv")
+test_that("Selects correct election type", {
+  expect_identical(get_file("Riksdagsval")[[1]], "Riksdagsval")
+  expect_identical(get_file("Landstingsval")[[1]], "Landstingsval")
+  expect_identical(get_file("Kommunval")[[1]], "Kommunval")
 })
 
-
+test_that("Selects correct data", {
+  expect_identical(get_file("Riksdagsval")[[2]][26:28,5], c(2278, 907, 502))
+  expect_length(get_file("Landstingsval")[[2]][,1], 289)
+  expect_equal(get_file("Kommunval")[[2]][104:105,4], c("Lomma", "Svedala"))
+})
 
 
 test_that("get_file stops with argument error", {
-  expect_error(get_file(20))
-  expect_error(get_file(c(1,6)))
+  expect_error(get_file(c("Riksdagsval", "Landstingsval")))
   expect_error(get_file("Hello!"))
 })
-
 
--- lab5sarasara/tests/testthat/test_graph_election.R
+++ lab5sarasara/tests/testthat/test_graph_election.R
@@ -1,8 +1,19 @@
 context("graph_election")
 
 
+test_that("Correct labels", {
+  expect_identical(graph_election("Lund", "Riksdagsval")$labels$title, "Election results 2014 in municipality Lund")
+  expect_identical(graph_election("Lund", "Landstingsval")$labels$subtitle, "Landstingsval")
+  expect_identical(graph_election("Ystad", "Landstingsval")$labels$y, "Percent")
+})
+
+test_that("Data is correct", {
+  expect_identical(as.character(graph_election("Robertsfors", "Kommunval")$data[1:4,1]), 
+                   c("Moderaterna", "Centerpartiet", "Folkpartiet", "Kristedemokraterna"))
+  expect_identical(graph_election("Robertsfors", "Riksdagsval")$data[2:5,2], c(16.77, 2.88, 4.59, 42.31))
+})
 
 test_that("grap_election stops with error", {
-  expect_error(graph_election("Sara"))
-  expect_error(graph_elction(c("Mora","Orsa")))
+  expect_error(graph_election("Sara", "Riksdagsval"))
+  expect_error(graph_elction(c("Mora","Orsa"), "Riksdagsval"))
 })
